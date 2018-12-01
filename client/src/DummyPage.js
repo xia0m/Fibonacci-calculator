078433b33820d38664be552257a9e1b7f6f73a35
xia0m <alexma325@gmail.com>
2018-12-01 11:37:48 -0800

    created client side react code

---
+++ client/src/DummyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default ()=>{
+  return (
+    <div>
+      Dummy Page!
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
