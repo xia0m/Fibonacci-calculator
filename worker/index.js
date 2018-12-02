bef1b5afbb82310a806dd2e5e5e307e05c6671f8
xia0m <xia0m@Zemins-MacBook-Pro.local>
2018-12-01 23:25:58 -0800

    fixed bug in index.js in worker folder

--- worker/index.js
+++ worker/index.js
@@ -16,7 +16,7 @@ function fib(index){
 }
 
 sub.on('message',(channel,message)=>{
-  redisClient.hset('value',message,fib(parseInt(message)));
+  redisClient.hset('values',message,fib(parseInt(message)));
 });
 
 sub.subscribe('insert');
